0a7a4727aec23decf7dc7fdf2512699acd5f6a4b
chenby@yctu.edu.cn <cby8612>
2023-07-19 23:30:28 +0800

    比赛

--- backend/apps/blueprint.py
+++ backend/apps/blueprint.py
@@ -2,9 +2,11 @@ from apps import app
 from apps.auth import auth
 
 # 微信登录验证
+from apps.competition import competition
 from apps.inventory import inventory
 from apps.test import test
 
 app.register_blueprint(auth, url_prefix='/auth', name='auth')
 app.register_blueprint(test,url_prefix='/test', name='test')
 app.register_blueprint(inventory,url_prefix='/inventory', name='inventory')
+app.register_blueprint(competition,url_prefix='/competition',name='competition')
