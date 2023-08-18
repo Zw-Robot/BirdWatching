9a435e9c519a1995fd0f2196dc6cb6884d07d43e
aaatsy <1977830401@qq.com>
2023-08-18 23:43:56 +0800

    111

--- BirdWatching/miniprogram/pages/mine/mine.ts
+++ BirdWatching/miniprogram/pages/mine/mine.ts
@@ -10,6 +10,13 @@ Page({
     nav_type:0,//默认选中第一个
     isFixed:false,//是否吸顶
     navTop:0,//nav菜单激励顶部距离
+
+    //用户信息
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
 
   changeType(e:any){
@@ -27,12 +34,15 @@ Page({
   },
 
   getAllBirdRecords:function(){
-    var date={
-      page: 1,
-      per_page: 20,
-    }
-    get_all_bird_records(date).then(res=>{
+    get_all_bird_records({}).then(res=>{
       console.log(res);
+    })
+  },
+
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs',
     })
   },
 
@@ -40,6 +50,33 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+
+  getUserProfile() {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e: any) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
 
   /**
